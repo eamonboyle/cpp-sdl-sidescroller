0d4b4506059b3a2835af507e7667311b3174ff30
eamonboyle <blaowskate@hotmail.com>
2020-01-22 22:37:46 +0000

    Add Asteroid class

---
+++ SideScroller/Asteroid.cpp
@@ -0,0 +1,15 @@
+#include "Asteroid.h"
+#include "SpriteComponent.h"
+#include "MoveComponent.h"
+#include "Game.h"
+
+Asteroid::Asteroid(Game* game)
+	: Actor(game)
+{
+
+
+}
+
+Asteroid::~Asteroid()
+{
+}
